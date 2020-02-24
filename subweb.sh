97e42e39de7b493a559b9e5a1abd1d51c25c07dd
lzdnico <596049368@qq.com>
2020-02-24 16:45:06 +0800

    fix running script without docker

--- subweb.sh
+++ subweb.sh
@@ -2,6 +2,10 @@
 echo "-----------------------------------Start-------------------------------------"
 chmod 777 /root/subweb/config/subconverter
 echo "-------------------------------------While--------------------------------------------"
+
+cd /root/subweb
+python3 api.py &   #运行web服务
+
 while true
 do
     proc_name="subconverter"        #进程名
@@ -17,19 +21,6 @@ do
         cd /root/subweb/config
         ./subconverter &
     fi
-    proc_name="python3"        #进程名
-    proc_num()                      #查询进程数量
-    {
-        num=`ps -ef | grep $proc_name | grep -v grep | wc -l`
-        return $num
-    }
-    proc_num  
-    number=$?                       #获取进程数量
-    if [ $number -eq 0 ]            #如果进程数量为0
-    then                            #重新启动服务器，或者扩展其它内容。
-        python3 /root/subweb/api.py &   #运行web服务
-    fi
-
     sleep 60s
 echo "------------------------------------Sucess----------------------------------------"
 done
